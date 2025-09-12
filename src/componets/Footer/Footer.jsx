a839f0919cf16924e75039ec6344dcf951e474da
deondre butler <deondrebutler20@gmail.com>
2025-09-12 15:52:32 -0400

    finished base project

---
+++ src/componets/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "../Footer/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__name">Develop by Deondre Butler</p>
+      <p className="footer__date">2025</p>
+    </div>
+  );
+}
+
+export default Footer;
